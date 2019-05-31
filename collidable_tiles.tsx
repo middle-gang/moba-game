c817586d92596b1993f054e7e6c650d14cbdd9fa
Nevaeh828 <49756646+Nevaeh828@users.noreply.github.com>
2019-05-31 23:43:31 +0800

    zhw 3
    素材资源更新，包括炮兵和塔的png

--- collidable_tiles.tsx
+++ collidable_tiles.tsx
@@ -1,9 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="collidable_tiles" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="code/ch13/13.4.4/Resources/map/collidable_tiles.jpg" width="32" height="32"/>
- <tile id="0">
-  <properties>
-   <property name="Collidable" type="bool" value="true"/>
-  </properties>
- </tile>
+ <image source="collidable_tiles.jpg" width="32" height="32"/>
 </tileset>
